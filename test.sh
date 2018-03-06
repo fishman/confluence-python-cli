1729483cd7e2522e532a5c614598d4fede286213
Reza Jelveh <toyb0mb@gmx.net>
2018-03-06 11:31:14 +0100

    updates

--- test.sh
+++ test.sh
@@ -19,7 +19,7 @@ createmetadatapage() {
 createdocument() {
   DOKUMENTEID=$(./confluence.py -w http://localhost:8090 -u $USER -p $PASS listpages | grep "$1" | head -n 1 | cut -d, -f 1)
   LABEL=$3
-  ./confluence.py -w http://localhost:8090 -u $USER -p $PASS addpage -n "$2" -s $SPACEKEY -P $DOKUMENTEID
+  ./confluence.py -w http://localhost:8090 -u $USER -p $PASS addpage -n "$2" -s $SPACEKEY -P $DOKUMENTEID -f templates/pagedefaultcontent.txt
   ./confluence.py -w http://localhost:8090 -u $USER -p $PASS updatepage -n "$2" -s $SPACEKEY -P $DOKUMENTEID -f templates/pagedefaultcontent.txt -l "$LABEL"
 }
 
@@ -39,14 +39,20 @@ createdocument "00 Dokumente" "11 Protokoll zur Management Bewertung" "dokumenta
 createdocument "00 Dokumente" "12 Verfahren zu Korrekturmassnahmen" "dokumentation"
 
 
-# DOKUMENTEID=$(./confluence.py -w http://localhost:8090 -u $USER -p $PASS listpages | grep "00 Dokumente" | head -n 1 | cut -d, -f 1)
-# ./confluence.py -w http://localhost:8090 -u $USER -p $PASS addpage -n '00 Verfahren zur Lenkung von Dokumenten und Aufzeichnungen' -s $SPACEKEY -P $DOKUMENTEID
-# ./confluence.py -w http://localhost:8090 -u $USER -p $PASS updatepage -n '00 Verfahren zur Lenkung von Dokumenten und Aufzeichnungen' -s $SPACEKEY -P $DOKUMENTEID -f templates/pagedefaultcontent.txt -l dokumentation
-# ./confluence.py -w http://localhost:8090 -u $USER -p $PASS addpage -n '01 Projektplan' -s $SPACEKEY -P $DOKUMENTEID
-
-
 createmetadatapage "01 Anhang" "anhang"
-# DOKUMENTEID=$(./confluence.py -w http://localhost:8090 -u $USER -p $PASS listpages | grep "01 Anhang" | head -n 1 | cut -d, -f 1)
-# LABEL=anhang
-# sed -e "s/%LABEL%/$LABEL/" -e "s/%PAGEID%/$DOKUMENTEID/" templates/pagemetadataoverview.txt > tmp/pagemetadataoverview.txt
-# ./confluence.py -w http://localhost:8090 -u $USER -p $PASS updatepage -n '01 Anhang' -s $SPACEKEY -P $PARENTPAGE -f tmp/pagemetadataoverview.txt -l dokumentation
+createdocument "01 Anhang" "06 Organisation der Informationssicherheit" "anhang"
+createdocument "01 Anhang" "07 Personelle Sicherheit" "anhang"
+createdocument "01 Anhang" "08 Management von Werten" "anhang"
+createdocument "01 Anhang" "09 Zugangskontrolle" "anhang"
+createdocument "01 Anhang" "10 Kryptographie" "anhang"
+createdocument "01 Anhang" "11 Sicherheit von Koerper und Umfeld" "anhang"
+createdocument "01 Anhang" "12 Betriebssicherheit" "anhang"
+createdocument "01 Anhang" "13 Kommunikationssicherheit" "anhang"
+createdocument "01 Anhang" "14 Systembeschaffung Entwicklung und" "anhang"
+createdocument "01 Anhang" "15 Beziehungen zu Lieferanten" "anhang"
+createdocument "01 Anhang" "16 Informationssicherheits Vorfallmanagement" "anhang"
+createdocument "01 Anhang" "17 a Sicherstellung des Geschaeftsbetriebs Richtlinie" "anhang"
+createdocument "01 Anhang" "17 b Sicherstellung des Geschaeftsbetriebs Geschaeftsauswirkungsanalyse" "anhang"
+createdocument "01 Anhang" "17 c Sicherstellung des Geschaeftsbetriebs Strategie" "anhang"
+createdocument "01 Anhang" "17 d Sicherstellung des Geschaeftsbetriebs Plan" "anhang"
+createdocument "01 Anhang" "17 e Sicherstellung des Geschaeftsbetriebs Weiteres" "anhang"
